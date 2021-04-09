dee0b75c49b8d24238dec93b591504fc58aa2592
bimalendu <bimalendu.behera@gmail.com>
2021-04-10 01:10:28 +0530

    Added web-server

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,35 +1,23 @@
-const request = require('request')
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-// const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/37.8267,-112'
+const address = process.argv[2]
 
-// request({ url: url, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather service!')
-//     } else if (response.body.error) {
-//         console.log('Unable to find location')
-//     } else {
-//         console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')   
-//     }
-// })
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return console.log(error)
+        }
 
-// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
 
-// request({ url: geocodeURL, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to location services!')
-//     } else if (response.body.features.length === 0) {
-//         console.log('Unable to find location. Try another search.')
-//     } else {
-//         const latitude = response.body.features[0].center[0]
-//         const longitude = response.body.features[0].center[1]
-//         console.log(latitude, longitude)
-//     }
-// })
-
-
-
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+            console.log(location)
+            console.log(forecastData)
+        })
+    })
+}
